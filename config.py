cb2215448c04d278bd5af421273321e46a1eabe9
jaideeprandhawa <jaideeprandhawa220@gmail.com>
2025-02-08 13:39:10 +0530

    gngv

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///C:/Users/Navkiran Kaur/OneDrive/Desktop/EduAI/instance/database.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///C:/Users/jaide/EduAI-Hackathon-/instance/database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
