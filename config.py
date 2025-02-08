43696f03fc333bb14f9adfc1f94b07595d397bc1
jaideeprandhawa <jaideeprandhawa220@gmail.com>
2025-02-08 15:30:16 +0530

    dvd

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(32)
-    SQLALCHEMY_DATABASE_URI = "sqlite:///C:/Users/Navkiran Kaur/OneDrive/Desktop/EduAI Hackathon/instance/database.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///C:/Users/jaide/EduAI-Hackathon-/instance/database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
